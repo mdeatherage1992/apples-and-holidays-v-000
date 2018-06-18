f04e9ae7bc6642332518aa17981338c2bdbd7cea
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 14:42:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season,holiday|
   holiday.each do |others,supplies|
+    binding.pry
     new_season = season.to_s.capitalize!
     others = holiday.to_s.capitalize!
     new_supplies = supplies.join(', ')
