619ac03b5284191db78f261bda952159b6428298
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 15:08:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,9 +94,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |key,value|
-    if value == "BBQ"
-      return key
+  holiday_hash.each do |season,holidays|
+    holidays.each do |key,value|
+      if value == "BBQ"
+        return key
+      end
     end
   end
 end
