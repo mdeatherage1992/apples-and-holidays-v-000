e072b249bf6ce8faca5bcbf5d02c9b2f4abc9f1a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 13:56:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ holiday_hash.each do |season,holiday,supplies|
   new_season = season.to_s.capitalize!
   new_holiday = holiday.to_s.capitalize!
   new_supplies = supplies.to_s
-  puts "#{new_season}: #{new_holiday}: #{new_supplies}"
+  puts "#{new_season}:"
 end
 end
 
