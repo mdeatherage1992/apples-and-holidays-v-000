b03a43d91b77bd00c002b51832044badb447682a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 12:48:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
     }
   }
 holiday_hash[:spring][:memorial_day] = ["Grill"]
-holiday_hash[:spring][:memorial_day] << "Table Cloth"
+holiday_hash[:spring][:memorial_day] = "Table Cloth"
 end
 
 
