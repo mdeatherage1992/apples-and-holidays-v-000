fee266f4d59ba801d6488c4ba6ccc3ec22fcc924
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 13:24:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+holiday_hash.each do |season,holiday,supplies|
+  puts season: holiday: supplies 
 
 end
 
